cfc5361ea5d2b676b499587246ea158c3fd4e2e4
TraffiXTon <traffixton@gmail.com>
2024-07-17 18:31:02 +0100

    Tasks page + minor changes on friends page

--- src/components/cards/TasksBoard.tsx
+++ src/components/cards/TasksBoard.tsx
@@ -1,31 +1,45 @@
 import TasksCard from "../cards/TasksCard.tsx";
-import {FC} from "react";
+import {useEffect, useState} from "react";
+import {ITaskApiResponse} from "../types/types.tsx";
+import fetchTasks from "../fetches/fetchTasks.ts";
 
-interface TaskBoardProps {
-    category: string;
-}
 
-const TasksBoard: FC<TaskBoardProps> = ({category}) => {
+const TasksBoard = () => {
 
-    const avatarUrl = new URL("/imgs/insta.svg", import.meta.url).href;
-    const avatarUrl2 = new URL("/imgs/telegram.jpg", import.meta.url).href;
-    const avatarUrl3 = new URL("/imgs/bradpitt.jpg", import.meta.url).href;
-    // фетчу апиху
-    // заполняю cardsData
-    // внизу мапом отрисовываю
-    const cardsData = [
-        {avatar: avatarUrl2, taskTitle: "Show tits", taskProfit: "300 KB", completed: true},
-        {avatar: avatarUrl, taskTitle: "Show pussy", taskProfit: "300 GB", completed: false},
-        {avatar: avatarUrl3, taskTitle: "Show ass", taskProfit: "300 MB", completed: true}
-    ]
+    // const avatarUrl = new URL("/imgs/insta.svg", import.meta.url).href;
+    // const avatarUrl2 = new URL("/imgs/telegram.jpg", import.meta.url).href;
+    // const avatarUrl3 = new URL("/imgs/bradpitt.jpg", import.meta.url).href;
+    // const cardsData = [
+    //     {avatar: avatarUrl2, taskTitle: "Show tits", taskProfit: "300 KB", completed: true},
+    //     {avatar: avatarUrl, taskTitle: "Show pussy", taskProfit: "300 GB", completed: false},
+    //     {avatar: avatarUrl3, taskTitle: "Show ass", taskProfit: "300 MB", completed: true}
+    // ]
+    const [cardData, setCardData] = useState<ITaskApiResponse | undefined>();
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                fetchTasks().then((data) => setCardData(data));
+                // const data = await fetchTasks();
+                // setCardData(data);
+                // if (Array.isArray(data)) {
+                //
+                // } else {
+                //     console.error("Fetched data is not an array:", data);
+                // }
+            } catch (error) {
+                console.error("Error fetching tasks:", error);
+            }
+        };
+        fetchData();
+    }, []);
+
     return (
         <div className={"TaskBoard pb-5"}>
-            <h1 className={"text-white text-2xl font-poppinsFont text-center mt-5 select-none"}>{category}</h1>
+            <h1 className={"text-white text-2xl font-poppinsFont text-center mt-5 select-none"}>Your tasks</h1>
             <div className="flex flex-col columns-1 px-6">
-
-                {cardsData.map((card, index) =>
-                    <TasksCard key={index} avatar={card.avatar} taskTitle={card.taskTitle} taskProfit={card.taskProfit}
-                               completed={card.completed}/>
+                {cardData?.results.map((card, index) =>
+                    <TasksCard key={index} avatar={card.icon} taskTitle={card.name} taskProfit={card.reward}
+                    category={card.category}/>
                 )}
             </div>
         </div>
