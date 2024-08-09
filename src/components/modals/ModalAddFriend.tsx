c5ba56908529b9421900d40a6f2e3349ba1608f8
TraffiXTon <traffixton@gmail.com>
2024-08-09 20:17:27 +0100

    Blur ModalAddFriend.tsx

--- src/components/modals/ModalAddFriend.tsx
+++ src/components/modals/ModalAddFriend.tsx
@@ -10,7 +10,7 @@ const ModalTasks: FC<ModalTasksProps> = ({showModal, closeModal, children}) => {
     if (!showModal) return null;
     return (
         <div className="fixed inset-6 flex place-items-center space-x-3 justify-center z-50 overflow-y-auto overflow-x-hidden">
-            <div className="fixed inset-0 bg-black bg-opacity-50 overscroll-hidden"
+            <div className="fixed inset-0 bg-black bg-opacity-50 overscroll-hidden backdrop-blur-sm"
                  onClick={closeModal}></div>
             <div className="bg-white relative text-center p-5 rounded-xl z-10">
                 {children}
