421fbe5d0eadc35ac367d98311bfdc8fb99ff9a2
TraffiXTon <traffixton@gmail.com>
2024-08-10 19:46:54 +0100

    Farm

--- src/components/cards/FarmBoard.tsx
+++ src/components/cards/FarmBoard.tsx
@@ -8,7 +8,7 @@ const FarmBoard = () => {
     return (
         <div className="flex flex-col justify-start">
             <div className={"flex justify-center items-center"}>
-                <h2 className={"text-center text-2xl font-poppinsFont text-white font-semibold mt-3"}>
+                <h2 className={"text-center text-2xl font-poppinsFont text-white font-medium mt-5"}>
                     Mining farm
                 </h2>
             </div>
